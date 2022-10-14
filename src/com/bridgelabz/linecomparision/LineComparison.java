229309c7245b3e142f9ec4558ee8e6797795bc09
PREETI2096 <preetirg96@gmail.com>
2022-10-14 10:20:32 +0530

    [preeti]:added uc1 calculation of length program

---
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.linecomparision;
+
+public class LineComparison {
+
+	//uc1 calculating the length of a line
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison computation program!");
+		int x1 = 20 , y1 = 6 ;
+		int x2 = 8 , y2 = 7;
+		
+		double length = Math.sqrt(Math.pow(x2- x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Length of line = " + length);
+
+	}
+
+}
